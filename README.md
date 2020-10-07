e6cedc92318c2292d4a666c5a161c030ff9b35f7
DalangLy <dalangly@gmail.com>
2020-10-07 17:10:23 +0700

    make login screen

--- README.md
+++ README.md
@@ -7,6 +7,15 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
 </p>
 
+## Libraries
+- [Laravel Excel] (https://laravel-excel.com/)
+- [Laravel Firebase] (https://github.com/kreait/firebase-php)
+
+## Install
+- composer require laravel/ui:^2.4
+- php artisan ui vue --auth
+- npm install
+
 ## About Laravel
 
 Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
--- routes/web.php
+++ routes/web.php
@@ -16,45 +16,52 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/test-drive', function () {
-    return redirect('/en/test-drive');
+Route::get('/', function(){
+    return view('origin');
 });
 
-Route::redirect('/', '/laos');//redirect homepage to /en
+//Route::get('/test-drive', function () {
+//    return redirect('/en/test-drive');
+//});
+//
+//Route::redirect('/', '/laos');//redirect homepage to /en
+//
+//Route::group(['prefix' => '{language}'], function(){
+//    Route::get('/', function($language=null){
+//        App::setLocale($language);
+//        return view('index');
+//    })->name('home');
+//
+//    Route::get('/contact', function($language=null){
+//        App::setLocale($language);
+//        return view('contact');
+//    })->name('contact');
+//
+//    Route::get('/product', function($language=null){
+//        App::setLocale($language);
+//        return view('product');
+//    })->name('product');
+//
+//    Route::get('/test-drive', function($language=null){
+//        App::setLocale($language);
+//        return view('test_drive');
+//    })->name('test_drive');
+//
+//    Route::get('/dealer', function($language=null){
+//        App::setLocale($language);
+//        return view('dealer');
+//    })->name('dealer');
+//
+//    Route::post('/send-email', [\App\Http\Controllers\ContactController::class, 'sendEmail'])->name('send-email');
+//    Route::post('/submit-test-drive', [TestDriveController::class, 'submitTestDriveEmail'])->name('submit-test-drive-email');
+//});
 
-Route::group(['prefix' => '{language}'], function(){
-    Route::get('/', function($language=null){
-        App::setLocale($language);
-        return view('index');
-    })->name('home');
-
-    Route::get('/contact', function($language=null){
-        App::setLocale($language);
-        return view('contact');
-    })->name('contact');
-
-    Route::get('/product', function($language=null){
-        App::setLocale($language);
-        return view('product');
-    })->name('product');
-
-    Route::get('/test-drive', function($language=null){
-        App::setLocale($language);
-        return view('test_drive');
-    })->name('test_drive');
-
-    Route::get('/dealer', function($language=null){
-        App::setLocale($language);
-        return view('dealer');
-    })->name('dealer');
-
-    Route::post('/send-email', [\App\Http\Controllers\ContactController::class, 'sendEmail'])->name('send-email');
-    Route::post('/submit-test-drive', [TestDriveController::class, 'submitTestDriveEmail'])->name('submit-test-drive-email');
-});
 
 
 
 
 
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
